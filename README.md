d312f063e3075d7be4afe1ddf65ebe7a6b4156f4
Christian Bepler <cgb45@duke.edu>
2025-04-29 20:15:16 -0400

    add issue summary

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ReadMeIssuesModel
+
+I believe the data collection is the main issue right now. The ReadMEs are not as nicely formatted as I expected. I wanted them to represent a sort of summary of the codebase but they would need extensive cleaning to actually work for that. The issues should also be further filtered to catch the more "project planning" issues.
--- modelUse.py
+++ modelUse.py
@@ -6,7 +6,13 @@ tokenizer = AutoTokenizer.from_pretrained("./ReadmeIssuesModel")
 texts = ["README: A python library for data visualization.", 
          "README: A tool to monitor system performance."]
 tokens = tokenizer(texts, return_tensors="pt", padding=True, truncation=True).to(model.device)
-outputs = model.generate(**tokens, max_new_tokens=150)
+outputs = model.generate(**tokens, 
+                         max_new_tokens=150, 
+                         do_sample=True,
+                         temperature=0.8,
+                         top_p=0.9,
+                         repetition_penalty=1.2,
+                         num_return_sequences=1)
 results = [tokenizer.decode(out, skip_special_tokens=True) for out in outputs]
 
 for r in results:
